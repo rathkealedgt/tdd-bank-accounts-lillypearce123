a760c4111afcd23da563922da76fa3622489c8cb
lillypearce123 <62360766+lillypearce123@users.noreply.github.com>
2020-05-11 16:00:24 +1200

    Test update

--- BankAccountTest/UnitTest1.vb
+++ BankAccountTest/UnitTest1.vb
@@ -1,9 +1,24 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
-
+Imports BankAcounts
 <TestClass()> Public Class UnitTest1
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestContructors1()
+
+
+        Dim AccounterHolder As String = "Ms I. N. Cognito"
+        Dim AccounterNumber As String = "ABCD 890111 11167890"
+        Dim InterestRate As Double = "4.3"
+        Dim Balance As Double = 10343.82
+        Dim country As String = "Isle of man"
+
+        Dim Account1 As New BankAccounts.BankAccount(AccounterHolder, AccounterNumber, Balance, InterestRate, country)
+        Dim Account2 As New BankAccounts.BankAccount(AccounterHolder, AccounterNumber, Balance, InterestRate)
+        Dim account3 As New BankAccounts.BankAccount(AccounterHolder, AccounterNumber, Balance)
+
+        Assert.num(Account1)
+        Assert.num(Account2)
+        Assert.num(account3)
     End Sub
 
 End Class
